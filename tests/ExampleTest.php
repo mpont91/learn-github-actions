d9285e40cf69559383dbecbf24789b2e2e35446a
Marc Pont <mpont91@gmail.com>
2022-01-13 15:40:10 +0100

    Add failing test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -18,4 +18,9 @@ class ExampleTest extends TestCase
             $this->app->version(), $this->response->getContent()
         );
     }
+
+    public function testFailing()
+    {
+        $this->assertEquals(true, false);
+    }
 }
